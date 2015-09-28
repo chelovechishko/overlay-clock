db7c93769d2a8b6ea7dc31d171fd09926035fae5
Chelovechishko <xaxaxa667@gmail.com>
2015-09-28 21:43:41 +0800

    Rewrite clock.js to reduce cpu and memory usage. Hope...

--- clock.js
+++ clock.js
@@ -6,10 +6,13 @@ overlay_clock.style.padding = "6px";
 overlay_clock.style.position = "fixed";
 overlay_clock.style.zIndex = "667";
 
-overlay_clock.onclick = function() {this.style.display = 'none'};
 document.body.appendChild(overlay_clock);
 
-mychromeclockDisp();
+
+overlay_clock.onclick = function() {
+	overlay_clock.style.display = 'none';
+	setTimeout(function() { overlay_clock.style.display = 'block' }, 10000);
+};
 
 function is_fullscreen() {
     // This is the q&d solution. The "right" way is: http://stackoverflow.com/a/20310858/955670
@@ -17,16 +20,13 @@ function is_fullscreen() {
     return ret;
 }
 
-// set by options
-var mil_time;
-
-function time_string() {
+function new_time() {
 	var now = new Date();
 	var hh = ('0' + now.getHours()).slice(-2);
 	var mm = ('0' + now.getMinutes()).slice(-2);
 	apm = '';
 
-	if (!mil_time) {
+	if (!my_overlay_clock_mil_time) {
 		if (hh < 13) {
 			apm = ' am'
 		}
@@ -40,37 +40,46 @@ function time_string() {
 	return hh + ':' + mm + apm;
 }
 
-function init_options(on_options_ready) {
-	chrome.storage.sync.get(document.clock_defaults, on_options_ready);
-}
+function create_clock(values) {
+	// using global variable to use it in new_time() and update_time_on_clock()
+	window.my_overlay_clock_fullscreen_only = values["fullscreen_only"];
+	window.my_overlay_clock_mil_time = values["mil_time"];
 
-function mychromeclockDisp() {
-	init_options(options_ready);
-}
+	my_overlay_clock_fg_color = values['fg_color'];
+	my_overlay_clock_bg_color = values['bg_color'];
+	my_overlay_clock_opacity = values['bg_opacity'];
+	my_overlay_clock_font_size = values['font_size'];
+	my_overlay_clock_font_family = values['font_family'];
+	my_overlay_clock_style_right = values['style_right'];
+	my_overlay_clock_style_top = values['style_top'];
 
-function options_ready(values) {
-	var fullscreen_only = values["fullscreen_only"];
-	mil_time = values["mil_time"];
-	var fg_color = values['fg_color'];
-	var bg_color = values['bg_color'];
-	var opacity = values['bg_opacity'];
-	var font_size = values['font_size'];
-	var font_family = values['font_family'];
-	var style_right = values['style_right'];
-	var style_top = values['style_top'];
-
-	var overlay_clock = document.getElementById("mychromeclock");
-	if (!fullscreen_only || is_fullscreen()) {
-		overlay_clock.textContent = time_string();
-		overlay_clock.style.color = fg_color;
-		overlay_clock.style.backgroundColor = bg_color;
-		overlay_clock.style.opacity = opacity;
-		overlay_clock.style.fontSize = font_size;
-		overlay_clock.style.fontFamily = font_family;
-		overlay_clock.style.right = style_right;
-		overlay_clock.style.top = style_top;
+	if (!my_overlay_clock_fullscreen_only || is_fullscreen()) {
+		overlay_clock.textContent = new_time();
+		overlay_clock.style.color = my_overlay_clock_fg_color;
+		overlay_clock.style.backgroundColor = my_overlay_clock_bg_color;
+		overlay_clock.style.opacity = my_overlay_clock_opacity;
+		overlay_clock.style.fontSize = my_overlay_clock_font_size;
+		overlay_clock.style.fontFamily = my_overlay_clock_font_family;
+		overlay_clock.style.right = my_overlay_clock_style_right;
+		overlay_clock.style.top = my_overlay_clock_style_top;
 		overlay_clock.style.display = 'block';
 	} else overlay_clock.style.display = 'none';
-
-	setTimeout(mychromeclockDisp, 15000); // 15s
 }
+
+function call_update_time_on_clock() {
+	setTimeout(update_time_on_clock, 10000);
+	setTimeout(call_update_time_on_clock, 10000);
+}
+
+function update_time_on_clock() {
+	if (!my_overlay_clock_fullscreen_only || is_fullscreen()) {
+		overlay_clock.textContent = new_time();
+		overlay_clock.style.display = 'block';
+	} else overlay_clock.style.display = 'none';
+}
+
+
+// Load defaults values and transfer them to function create_clock()
+chrome.storage.sync.get(document.clock_defaults, create_clock);
+
+call_update_time_on_clock();
